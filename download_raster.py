d8af95fde083b3731ec1fae6ee7c1ebace3bea5b
xinwei89 <113005350+xinwei89@users.noreply.github.com>
2023-10-17 11:35:30 +1100

    don't download already downloaded sa1 raster

--- download_raster.py
+++ download_raster.py
@@ -19,20 +19,31 @@ def main():
     # shapefile_path = "../data/SA1_2021_AUST_SHP_GDA2020/SA1_2021_AUST_GDA2020.shp"
     # gdf = gpd.read_file(shapefile_path)
     # print(gdf.crs)
-
     # # getting greater sydney only SA1 polygons
     # nsw_gdf = gdf[gdf["GCC_CODE21"] == "1GSYD"]
-
     # nsw_gdf.loc[:, "xmin"] = nsw_gdf["geometry"].bounds["minx"]
     # nsw_gdf.loc[:, "ymin"] = nsw_gdf["geometry"].bounds["miny"]
     # nsw_gdf.loc[:, "xmax"] = nsw_gdf["geometry"].bounds["maxx"]
     # nsw_gdf.loc[:, "ymax"] = nsw_gdf["geometry"].bounds["maxy"]
-
     # nsw_gdf = nsw_gdf[["SA1_CODE21", "xmin", "ymin", "xmax", "ymax", "geometry"]]
 
-    nsw_gdf = gpd.read_file("filtered_nsw_sa1.geojson")
+    # Don't download rasters that is already downloaded, so find them first and remove from nsw_gdf
+    root_directory = "."
+    extracted_sa1 = set()
 
-    # Define the folder path
+    for root, dirs, files in os.walk(root_directory):
+        for dir in dirs:
+            if dir.startswith("coverage"):
+                dir_path = os.path.join(root, dir)
+                for file in os.listdir(dir_path):
+                    extracted_sa1.add(file.split("_")[0])
+
+    extracted_sa1 = list(extracted_sa1)
+    # sa1 with building annotations over 10%
+    nsw_gdf = gpd.read_file("filtered_nsw_sa1.geojson")
+    nsw_gdf = nsw_gdf[~nsw_gdf["SA1_CODE21"].isin(extracted_sa1)]
+
+    # Define the folder path to sa1 codes and respective percentage of building annotation coverage
     folder_path = "draft"
 
     for filename in os.listdir(folder_path):

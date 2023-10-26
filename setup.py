10b027a564811e0743447ec5d3738d46bf75407e
Henry Lydecker <henry.lydecker@gmail.com>
2023-10-27 09:36:47 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='aerial_conversion',
     description='Data interchange utilities for going from geospatial data formats to computer vision formats and back',
-    author='Sahand Vahdnia, Henry Lydecker, Xinwei Luo, Thomas Mauch',
+    author='Sahand Vahidnia, Henry Lydecker, Xinwei Luo, Thomas Mauch',
     author_email='henry.lydecker@gmail.com',
     packages=['aerial_conversion']
 )

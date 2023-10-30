397254d60b37c0daee659be2fe8e9b43f32bb0dc
Sahand <sahand.vahindia@sydney.edu.au>
2023-10-30 16:14:55 +1100

    added default info json + pre-commit

--- scripts/batch_geojson2coco.py
+++ scripts/batch_geojson2coco.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-"""This script supports batch conversion of paired geojson and raster data into a series of COCO datasets."""
-import os
-import subprocess
+"""This script supports batch conversion of paired geojson and raster data into
+a series of COCO datasets."""
 import argparse
 import json
+import os
+import subprocess
 
 from pycocotools.coco import COCO
 
-def main(args):
 
-    """
-    Convert raster and vector pairs to COCO JSON format.
+def main(args):
+    """Convert raster and vector pairs to COCO JSON format.
 
     Args:
         args: Command-line arguments.
@@ -22,7 +22,7 @@ def main(args):
         # Convert raster and vector pairs with concatenation
         python batch_geojson2coco.py --raster-dir /path/to/raster_dir --vector-dir /path/to/vector_dir --output-dir /path/to/output_dir --concatenate
     """
-    
+
     # Specify the output directory
     output_dir = args.output_dir
 
@@ -31,12 +31,12 @@ def main(args):
     # Iterate over the raster directory
     for raster_file in os.listdir(args.raster_dir):
         # Check if the file is a GeoTIFF
-        if raster_file.endswith('.tif'):
+        if raster_file.endswith(".tif"):
             # Get the file name without extension
             file_name = os.path.splitext(raster_file)[0]
 
             # Construct the vector file name
-            vector_file = file_name + '.geojson'
+            vector_file = file_name + ".geojson"
             vector_path = os.path.join(args.vector_dir, vector_file)
 
             # Check if the vector file exists
@@ -46,18 +46,38 @@ def main(args):
                 os.makedirs(pair_output_dir, exist_ok=True)
 
                 # Specify the output JSON file path
-                json_file = os.path.join(pair_output_dir, 'coco_from_gis.json')
+                json_file = os.path.join(pair_output_dir, "coco_from_gis.json")
+
+                if not args.info:
+                    # create an empty info.json file and set info_file to it
+                    info_file = os.path.join(output_dir, "info.json")
+                    with open(info_file, "w") as f:
+                        json.dump({}, f)
+                else:
+                    info_file = os.path.join(args.info, f"{file_name}.json")
+
+                print(
+                    f"Processing {vector_file} | {raster_file} | {info_file} > > > > {json_file}"
+                )
 
                 # Construct the command
                 command = [
-                    'python', 'scripts/geojson2coco.py',
-                    '--raster-file', os.path.join(args.raster_dir, raster_file),
-                    '--polygon-file', vector_path,
-                    '--tile-dir', pair_output_dir,
-                    '--json-name', json_file,
-                    '--info', os.path.join(pair_output_dir, 'info.json'),
-                    '--tile-size', str(args.tile_size),
-                    '--class-column', args.class_column
+                    "python",
+                    "scripts/geojson2coco.py",
+                    "--raster-file",
+                    os.path.join(args.raster_dir, raster_file),
+                    "--polygon-file",
+                    vector_path,
+                    "--tile-dir",
+                    pair_output_dir,
+                    "--json-name",
+                    json_file,
+                    "--info",
+                    info_file,
+                    "--tile-size",
+                    str(args.tile_size),
+                    "--class-column",
+                    args.class_column,
                 ]
 
                 try:
@@ -68,45 +88,68 @@ def main(args):
 
                 # Add the generated COCO dataset to the list
                 individual_coco_datasets.append(json_file)
-    
+
     # Generate markdown output for individual COCO datasets
-    print('Running geojson2coco.py over raster and vector pairs:')
+    print("Running geojson2coco.py over raster and vector pairs:")
     print()
-    print('| Raster File | Vector File | JSON File |')
-    print('|-------------|-------------|-----------|')
+    print("| Raster File | Vector File | JSON File |")
+    print("|-------------|-------------|-----------|")
     for coco_file in individual_coco_datasets:
         pair_dir = os.path.dirname(coco_file)
-        raster_file = os.path.basename(pair_dir) + '.tif'
-        vector_file = os.path.basename(pair_dir) + '.geojson'
-        print(f'| {raster_file} | {vector_file} | {coco_file} |')
+        raster_file = os.path.basename(pair_dir) + ".tif"
+        vector_file = os.path.basename(pair_dir) + ".geojson"
+        print(f"| {raster_file} | {vector_file} | {coco_file} |")
 
     # Concatenate COCO datasets if the --concatenate argument is enabled
     if args.concatenate:
         concatenated_coco = COCO()  # Create a new COCO dataset
         for coco_file in individual_coco_datasets:
-            with open(coco_file, 'r') as f:
+            with open(coco_file, "r") as f:
                 dataset = json.load(f)
                 concatenated_coco.dataset.update(dataset)
 
         # Specify the output directory for the concatenated dataset
-        concatenated_output_dir = os.path.join(args.output_dir, 'concatenated')
+        concatenated_output_dir = os.path.join(args.output_dir, "concatenated")
         os.makedirs(concatenated_output_dir, exist_ok=True)
 
         # Save the concatenated COCO dataset
-        concatenated_json_file = os.path.join(concatenated_output_dir, 'concatenated_coco.json')
-        with open(concatenated_json_file, 'w') as f:
+        concatenated_json_file = os.path.join(
+            concatenated_output_dir, "concatenated_coco.json"
+        )
+        with open(concatenated_json_file, "w") as f:
             json.dump(concatenated_coco.dataset, f)
 
-        print(f'\nConcatenated COCO dataset saved to: {concatenated_json_file}')
+        print(f"\nConcatenated COCO dataset saved to: {concatenated_json_file}")
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Convert raster and vector pairs to COCO JSON format.")
-    parser.add_argument("--raster-dir", required=True, help="Path to the raster directory.")
-    parser.add_argument("--vector-dir", required=True, help="Path to the vector directory.")
-    parser.add_argument("--output-dir", required=True, help="Path to the output directory.")
-    parser.add_argument("--tile-size", type=int, default=100, help="Tile width/height in meters.")
-    parser.add_argument("--class-column", default="trees", help="Column name in GeoJSON for classes.")
-    parser.add_argument("--concatenate", action="store_true", help="Concatenate individual COCO datasets into one.")
+    parser = argparse.ArgumentParser(
+        description="Convert raster and vector pairs to COCO JSON format."
+    )
+    parser.add_argument(
+        "--raster-dir", required=True, help="Path to the raster directory."
+    )
+    parser.add_argument(
+        "--vector-dir", required=True, help="Path to the vector directory."
+    )
+    parser.add_argument(
+        "--output-dir", required=True, help="Path to the output directory."
+    )
+    parser.add_argument(
+        "--tile-size", type=int, default=100, help="Tile width/height in meters."
+    )
+    parser.add_argument(
+        "--class-column", default="trees", help="Column name in GeoJSON for classes."
+    )
+    parser.add_argument(
+        "--concatenate",
+        action="store_true",
+        help="Concatenate individual COCO datasets into one.",
+    )
+    parser.add_argument(
+        "--info",
+        help="Path to the info JSON file. Either leave empty, or put a folder path., containing info.json for each raster, with the same names as the rasters.",
+    )
 
     args = parser.parse_args()
     main(args)

60ef32b0ef0b7fa8fccb6778eb588504240f5d66
Tom Mauch <thomas.mauch@sydney.edu.au>
2023-11-16 13:28:39 +1100

    Remove unused setup.py.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='aerial_conversion',
-    description='Data interchange utilities for going from geospatial data formats to computer vision formats and back',
-    author='Sahand Vahidnia, Henry Lydecker, Xinwei Luo, Thomas Mauch',
-    author_email='henry.lydecker@gmail.com',
-    packages=['aerial_conversion']
-)

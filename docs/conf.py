391e7db230e38620ab4b1a410eafe50d426773cd
Sahand <sahand.vahindia@sydney.edu.au>
2023-12-04 17:40:07 +1100

    output dir for doc fix

--- docs/conf.py
+++ docs/conf.py
@@ -42,8 +42,7 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme_path = ["."]
-
+html_output_directory = "."
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for

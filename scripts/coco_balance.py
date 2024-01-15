ae65a6b1c06805ed81f676b08fa612484c971597
Sahand <sahand.vahindia@sydney.edu.au>
2024-01-15 16:28:20 +1100

    added the ability to manipulate categories by removing them from the ds

--- scripts/coco_balance.py
+++ scripts/coco_balance.py
@@ -3,8 +3,8 @@
 import argparse
 import json
 
-# from tqdm import tqdm
 import pandas as pd
+from tqdm import tqdm
 
 # import os
 
@@ -31,16 +31,74 @@ def stats(json_data):
     print(cats_unique_per_image)
     print(cats_stat_per_image)
 
+    cats_unique_per_image = cats_unique_per_image.reset_index()
+    cats_unique_per_image = cats_unique_per_image.groupby("category_id").count()
+    cats_unique_per_image = cats_unique_per_image.reset_index()
+    cats_unique_per_image.columns = ["cats", "images"]
+    print("stats for the diversity of cats in images")
+    print(cats_unique_per_image)
+
+
+def isolate_cat(json_data: dict, cat_ids: list):
+    # Limit the annotations to the given categories
+    print("Limiting the annotations to the given categories")
+    annotations = json_data["annotations"]
+    new_annotations = []
+    for annot in tqdm(annotations):
+        if annot["category_id"] in cat_ids:
+            new_annotations.append(annot)
+    print("Removed {} annotations".format(len(annotations) - len(new_annotations)))
+    assert len(new_annotations) > 0, "No annotations left after filtering"
+
+    json_data["annotations"] = new_annotations
+
+    # Limit the categories to the given categories
+    print("Limiting the categories to the given categories")
+    categories = json_data["categories"]
+    new_categories = []
+    for cat in tqdm(categories):
+        if cat["id"] in cat_ids:
+            new_categories.append(cat)
+    print("Removed {} categories".format(len(categories) - len(new_categories)))
+    json_data["categories"] = new_categories
+
+    # Remove the images if not referenced in annotations
+    print("Removing the images if not referenced in annotations")
+    image_ids = []
+    for annot in annotations:
+        image_ids.append(annot["image_id"])
+    image_ids = list(set(image_ids))
+    images = json_data["images"]
+    new_images = []
+    for image in tqdm(images):
+        if image["id"] in image_ids:
+            new_images.append(image)
+    print("Removed {} images".format(len(images) - len(new_images)))
+    json_data["images"] = new_images
+
+    return json_data
+
 
 def main(args=None):
     if args is None:
         args = parse_arguments(args)
 
     # Load the COCO json file
+    print("Loading the COCO json file")
     with open(args.json_path, "r") as f:
         json_data = json.load(f)
 
-    stats(json_data)
+    # stats(json_data)
+    if args.isolate_cat:
+        cats = args.isolate_cat.split(",")
+        if args.int_cats:
+            cats = [int(cat) for cat in cats]
+        json_data = isolate_cat(json_data, cats)
+
+    # Save the COCO json file
+    print("Saving the COCO json file")
+    with open(args.output_path, "w") as f:
+        json.dump(json_data, f, indent=2)
 
 
 def parse_arguments(args):
@@ -50,6 +108,17 @@ def parse_arguments(args):
     )
     parser.add_argument(
         "--output_path", "-o", type=str, help="Path to your output COCO json file"
+    )
+    parser.add_argument(
+        "--isolate_cat",
+        "-c",
+        type=str,
+        help="Comma separated list of category ids to isolate from to main dataset.",
+    )
+    parser.add_argument(
+        "--int_cats",
+        action=argparse.BooleanOptionalAction,
+        help="Set this flag if the categores are integers.",
     )
     parser.add_argument(
         "--augmentation_low",

7e793a2977caec5ab732afef935785223483038d
Tom Mauch <thomas.mauch@sydney.edu.au>
2024-01-19 09:40:36 +1100

    Add retries and a longer timeout to the requests.

--- scripts/get_raster_jpeg.py
+++ scripts/get_raster_jpeg.py
@@ -3,6 +3,7 @@ import os
 import time
 
 import geojson
+import requests
 from owslib.wms import WebMapService
 
 # Download a list of JPEG tiles from the NSW Six Maps Web Map Server.
@@ -18,6 +19,26 @@ INPUT_JSON = "GSU_grid.geojson"
 # GSU_grid.geojson is 300x300m
 SIZE = (4019, 4019)
 
+
+def request_image_from_server(wms_instance, output_file, attempts=3, **kwargs):
+    """Try to download image using defined WMS instance with multiple attempts.
+
+    Raise a `ReadTimeout` if the exception is reaised more than
+    `attempts` times. kwargs are passed to wms_instance.getmap
+    """
+    this_attempt = 1
+    while this_attempt <= attempts:
+        try:
+            image_request = wms_instance.getmap(**kwargs)
+            with open(output_file, "wb") as this_image:
+                this_image.write(image_request.read())
+            break
+        except requests.exceptions.ReadTimeout:
+            this_attempt += 1
+            if this_attempt > attempts:
+                raise
+
+
 with open(INPUT_JSON) as file:
     raw_geojson = geojson.load(file)
 
@@ -27,7 +48,7 @@ print(f"{len(geojson_feature_list)} tiles to download.")
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-wms = WebMapService(SIXMAPS_WMS_URL, version=SIXMAPS_WMS_VERSION)
+wms = WebMapService(SIXMAPS_WMS_URL, version=SIXMAPS_WMS_VERSION, timeout=60)
 
 for i, feature in enumerate(geojson_feature_list):
     this_id = feature["properties"]["id"]
@@ -40,12 +61,17 @@ for i, feature in enumerate(geojson_feature_list):
         print(f"Skipping Tile {i + 1}. File {output_file} exists.")
         continue
     print(f"Get tile {i + 1} of {num_tiles} -> {output_file}")
-    # st = time.time()
-    image_request = wms.getmap(
-        bbox=this_bbox, srs="EPSG:3857", layers=["0"], size=SIZE, format="image/jpeg"
+    st = time.time()
+    request_image_from_server(
+        wms,
+        output_file,
+        attempts=3,
+        bbox=this_bbox,
+        srs="EPSG:3857",
+        layers=["0"],
+        size=SIZE,
+        format="image/jpeg",
     )
-    with open(output_file, "wb") as this_image:
-        this_image.write(image_request.read())
-    # et = time.time()
+    et = time.time()
     # Benchmark
-    # print(et - st)
+    print(et - st)
